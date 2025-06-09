a6944b5ca360cf76028df46e33e3016cc7f0fc4c
hiqatech <hiqatech@gmail.com>
2025-06-09 15:54:31 +0200

    compare update

--- src/main/java/Common/Util.java
+++ src/main/java/Common/Util.java
@@ -13,109 +13,122 @@ public class Util {
 
     public static String matchingItem = "";
     public static List<String> whatIsNotMatching = new ArrayList<>();
+    public static List<String> expectedMatching = new ArrayList<>();
 
 
     public static void setList1(List<Map<String, String>> data) {
         List1 = data;
         System.out.println("Setting List1");
-        System.out.println("List1 = " + List1);
-        System.out.println("--------------------------------------------------");
+        for (Map map : List1) {
+            List<String> listHeaders = new ArrayList<String>(map.keySet());
+            List<String> listValues1 = new ArrayList<String>(map.values());
+            for (int i = 0; i < listHeaders.size(); i++) {
+                System.out.print(listHeaders.get(i) +
+                        " = " + listValues1.get(i) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------");
     }
 
     public static void setList2(List<Map<String, String>> data) {
         List2 = data;
         System.out.println("Setting List2");
-        System.out.println("List2 = " + List2);
-        System.out.println("--------------------------------------------------");
+        for (Map map : List2) {
+            List<String> listHeaders = new ArrayList<String>(map.keySet());
+            List<String> listValues2 = new ArrayList<String>(map.values());
+            for (int i = 0; i < listHeaders.size(); i++) {
+            System.out.print(listHeaders.get(i) +
+                    " = " + listValues2.get(i) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------");
     }
 
-    public static void compareLists(){
-        int map1LineNumber = 0;
-        int map2LineNumber = 0;
+    public static void compareLists() {
+        int list1LineNumber = 0;
+        int list2LineNumber = 0;
 
         System.out.println("Comparing Lists");
-        System.out.println("-Comparing List1 to List2");
+        System.out.println("------------------------");
+        System.out.println("Comparing List1 to List2");
 
         for (Map map1 : List1) {
-            System.out.println("Comparing Map1 Item" + map1LineNumber);
             List<String> listHeaders = new ArrayList<String>(map1.keySet());
             List<String> listValues1 = new ArrayList<String>(map1.values());
+            System.out.println("Comparing List1 Item" + list1LineNumber + " " + listValues1.getFirst());
             for (Map map2 : List2) {
-                System.out.println("- with Map2 Item" + map2LineNumber);
                 List<String> listValues2 = new ArrayList<String>(map2.values());
-                if(listValues1.getFirst().equals(listValues2.getFirst())) {
+                System.out.println("- with List2 Item" + list2LineNumber + " " + listValues2.getFirst());
+                if (listValues1.getFirst().equals(listValues2.getFirst())) {
                     System.out.println("Match found " +
                             listHeaders.getFirst() + " " + listValues1.getFirst());
-                    for(int i=1;i<listValues1.size();i++) {
-                        if(listValues1.get(i).equals(listValues2.get(i)))
-                        {System.out.println("Matching params found " +
-                                listHeaders.get(i) + " " + listValues1.get(i));
-                            matchingItem = "FOUND";}
-                        else {
+                    matchingItem = "FOUND";
+
+                    for (int i = 1; i < listValues1.size(); i++) {
+                        if (listValues1.get(i).equals(listValues2.get(i))) {
+                            System.out.println("Matching params found " +
+                                    listHeaders.get(i) + " " + listValues1.get(i));
+                        } else {
                             whatIsNotMatching.add("List1 Item " + listValues1.getFirst() +
-                                    " foud in List2 but not matching on "
+                                    " found in List2 but not matching on "
                                     + listHeaders.get(i) + " as "
                                     + listValues1.get(i) + " not equals " + listValues2.get(i));
                             System.out.println(whatIsNotMatching.getLast());
                         }
                     }
-
                 }
-                if(matchingItem.equals("FOUND")) break;
-                map2LineNumber++;
+                if (matchingItem.equals("FOUND")) break;
+                else list2LineNumber++;
             }
-            if(!matchingItem.equals("FOUND")) {
-                whatIsNotMatching.add("List 1 Item " + listValues1.getFirst() +  " not found in List2");
+            if (!matchingItem.equals("FOUND")) {
+                whatIsNotMatching.add("List1 Item " + listValues1.getFirst() + " not found in List2");
                 System.out.println(whatIsNotMatching.getLast());
             }
-            map2LineNumber = 0;
-            map1LineNumber++;
+            list2LineNumber = 0;
+            list1LineNumber++;
             matchingItem = "";
-            System.out.println("---");
+            System.out.println("------------------------");
         }
 
-        map1LineNumber = 0;
-        map2LineNumber = 0;
+        list1LineNumber = 0;
+        list2LineNumber = 0;
 
-        System.out.println("-Comparing List2 to List1");
+        System.out.println("------------------------");
+        System.out.println("Comparing List2 to List1");
 
         for (Map map2 : List2) {
-            System.out.println("Comparing Map2 Item" + map2LineNumber);
             List<String> listHeaders = new ArrayList<String>(map2.keySet());
-            List<String> listValues1 = new ArrayList<String>(map2.values());
+            List<String> listValues2 = new ArrayList<String>(map2.values());
+            System.out.println("Comparing List2 Item" + list2LineNumber + " " + listValues2.getFirst());
             for (Map map1 : List1) {
-                System.out.println("- with Map1 Item" + map1LineNumber);
-                List<String> listValues2 = new ArrayList<String>(map1.values());
-                if(listValues1.getFirst().equals(listValues2.getFirst())) {
-                    System.out.println("Match found " +
-                            listHeaders.getFirst() + " " + listValues1.getFirst());
-                    for(int i=1;i<listValues1.size();i++) {
-                        if(listValues1.get(i).equals(listValues2.get(i)))
-                            matchingItem = "FOUND";}
+                List<String> listValues1 = new ArrayList<String>(map1.values());
+                System.out.println("- with List1 Item" + list1LineNumber+ " " + listValues1.getFirst());
+                if (listValues2.getFirst().equals(listValues1.getFirst())) {
+                    matchingItem = "FOUND";
                 }
-                if(matchingItem.equals("FOUND")) break;
-                map1LineNumber++;
+                if (matchingItem.equals("FOUND")) break;
+                else list1LineNumber++;
             }
-            if(!matchingItem.equals("FOUND")) {
-                whatIsNotMatching.add("List2 Item" + listValues1.getFirst() +  " not found in List1");
+            if (!matchingItem.equals("FOUND")) {
+                whatIsNotMatching.add("List2 Item " + listValues2.getFirst() + " not found in List1");
                 System.out.println(whatIsNotMatching.getLast());
             }
-            map1LineNumber = 0;
-            map2LineNumber++;
+            list1LineNumber = 0;
+            list2LineNumber++;
             matchingItem = "";
-            System.out.println("----------------------");
+            System.out.println("-------------------------");
+            System.out.println("--------------------------------------------------");
         }
-
-        System.out.println("--------------------------------------------------");
     }
 
     public static void AssertLists() {
         System.out.println("Asserting Lists discrepancies");
+        System.out.println("Discrepancies : ");
         for(String notMatching : whatIsNotMatching) {
-            System.out.println("Discrepancies : ");
             System.out.println(notMatching);
-
         }
-        Assert.assertEquals(whatIsNotMatching.toString(), "");
+        Assert.assertEquals(whatIsNotMatching.toString(), expectedMatching.toString());
     }
 }
--- src/test/resources/ListsValidate.feature
+++ src/test/resources/ListsValidate.feature
@@ -2,17 +2,19 @@ Feature: Validate Lists
 
   @ListTests
 Scenario: Validate that two lists contain the same items with name, price, and category, regardless of order
-Given I have the following items in the first list:
-| name   | price | category |
-| apple  | 1.00  | fruit    |
-| orange | 1.50  | fruit    |
-| banana | 0.75  | fruit    |
-| kiwi | 1.50  | vegetable |
-And I have the following items in the second list:
-| name   | price | category |
-| banana | 0.70  | fruit    |
-| apple  | 1.00  | fruit    |
-| orange | 1.50  | vegetable |
-| berry | 1.50  | vegetable |
-When I compare both lists
-Then the lists should contain the same items with name, price, and category, regardless of order
+    Given I have the following items in the first list:
+      | name   | price | category |
+      | apple  | 1.00  | fruit    |
+      | orange | 1.50  | fruit    |
+      | banana | 0.75  | fruit    |
+      | kiwi | 1.50  | vegetable |
+      | melon | 1.00  | vegetable |
+    And I have the following items in the second list:
+      | name   | price | category |
+      | banana | 0.70  | fruit    |
+      | apple  | 1.00  | fruit    |
+      | orange | 1.50  | vegetable |
+      | berry | 1.50  | fruit |
+      | melon | 1.20  | fruit |
+    When I compare both lists
+    Then the lists should contain the same items with name, price, and category, regardless of order
